9bd407f3e4d359a3062ae8c7a19fa2a9e289d70d
nromsdahl <nevadaromsdahl@protonmail.com>
2024-06-15 11:37:10 -0500

    Update auto_atomic_list.ps1

--- auto_atomic_list.ps1
+++ auto_atomic_list.ps1
@@ -1,6 +1,9 @@
 # Define variables
 $repoUrl = "https://github.com/redcanaryco/atomic-red-team.git"
-$repoDirectory = "$pwd:\AtomicRedTeam"  # Change this to your desired directory
+$repoDirectory = "$pwd\AtomicRedTeam"  # Change this to your desired directory
+
+$repoUrlps = "https://github.com/redcanaryco/invoke-atomicredteam.git"
+$repoDirectoryps = "$pwd\InvokeAtomicRedTeam"
 
 # Example list of Atomic tests to run (replace with actual test names)
 $testList = @(
@@ -10,16 +13,31 @@ $testList = @(
     "T1218.010"
 )
 
+
+# Install powershell yaml dependencie
+
+function Install-Dependencies {
+    Write-Output "Installing dependencies..."
+    Install-Module -Name powershell-yaml -Force
+}
+
 # Function to clone the Atomic Red Team repository
 function Clone-AtomicRedTeamRepository {
     Write-Output "Cloning Atomic Red Team repository from $repoUrl..."
     git clone $repoUrl $repoDirectory
 }
 
+# Function to clone the Atomic Red Team repository
+function Clone-AtomicRedTeamRepositoryps {
+    Write-Output "Cloning Atomic Red Team repository from $repoUrlps..."
+    git clone $repoUrlps $repoDirectoryps
+}
+
+
 # Function to import the Invoke-AtomicRedTeam module
 function Import-AtomicRedTeamModule {
-    Write-Output "Importing Invoke-AtomicRedTeam module..."
-    Import-Module "$repoDirectory\atomics\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam.psd1"
+    Write-Output "Installing Invoke Atomic Red Team...."
+    Import-Module "$repoDirectoryps\Invoke-AtomicRedTeam.psd1"
 }
 
 # Function to run Atomic tests from a specified list
@@ -27,11 +45,12 @@ function Run-AtomicTests {
     param(
         [string]$testName
     )
+    $PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="$pwd\AtomicRedTeam\atomics"}
 
     Write-Output "Running Atomic test: $testName"
 
     try {
-        Invoke-AtomicTest -Path "$repoDirectory\atomics\$testName"
+        Invoke-AtomicTest $testName
         Write-Output "Successfully ran test: $testName"
     }
     catch {
@@ -41,8 +60,13 @@ function Run-AtomicTests {
 
 # Main script
 
+#Install deps
+Install-Module -Name powershell-yaml -Force
+
 # Step 1: Clone Atomic Red Team repository
 Clone-AtomicRedTeamRepository
+
+Clone-AtomicRedTeamRepositoryps
 
 # Step 2: Import Invoke-AtomicRedTeam module
 Import-AtomicRedTeamModule
